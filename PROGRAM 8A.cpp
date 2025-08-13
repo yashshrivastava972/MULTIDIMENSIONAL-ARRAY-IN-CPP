6a141c70cd22b77f4a10a2df6481d34bb96159e7
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-13 16:33:21 +0530

    Update PROGRAM 8A.cpp

--- PROGRAM 8A.cpp
+++ PROGRAM 8A.cpp
@@ -1,3 +1,10 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: TAKE MATRIX INPUT FROM USER AND DISPLAY IT (EXPERIMENT: 8A)
+
+// CODE: 
+
 #include <iostream>
 using namespace std;
 
@@ -34,20 +41,20 @@ int main() {
 
 // OUTPUT: 
 
-Enter number of rows: 3
-Enter number of columns: 3
-Enter matrix elements:
-Element [1][1]: 1
-Element [1][2]: 2
-Element [1][3]: 3
-Element [2][1]: 4
-Element [2][2]: 5
-Element [2][3]: 6
-Element [3][1]: 7
-Element [3][2]: 8
-Element [3][3]: 9
+// Enter number of rows: 3
+// Enter number of columns: 3
+// Enter matrix elements:
+// Element [1][1]: 1
+// Element [1][2]: 2
+// Element [1][3]: 3
+// Element [2][1]: 4
+// Element [2][2]: 5
+// Element [2][3]: 6
+// Element [3][1]: 7
+// Element [3][2]: 8
+// Element [3][3]: 9
 
-The matrix is:
-1	2	3	
-4	5	6	
-7	8	9	
+// The matrix is:
+// 1	2	3	
+// 4	5	6	
+// 7	8	9	
