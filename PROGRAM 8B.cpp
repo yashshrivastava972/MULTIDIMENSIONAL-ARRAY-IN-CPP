26a87a3df0d31d2321e0a4683fb71833cd16b0b5
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-13 15:40:36 +0530

    Create PROGRAM 8B.cpp

---
+++ PROGRAM 8B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[3][3], b[3][3], sum[3][3];
+
+    cout << "Enter first 3x3 matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Enter second 3x3 matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> b[i][j];
+        }
+    }
+
+    cout << "\nSum of matrices:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// OUTPUT:
+
+Enter first 3x3 matrix:
+1 2 3
+4 5 6
+7 8 9
+Enter second 3x3 matrix:
+9 8 7 
+6 5 4
+3 2 1
+
+Sum of matrices:
+10 10 10 
+10 10 10 
+10 10 10 
