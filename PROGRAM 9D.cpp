089f979cff1c4e3012b377927b48bfb2df670243
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-13 15:48:23 +0530

    Create PROGRAM 9D.cpp

---
+++ PROGRAM 9D.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3], sum = 0;
+
+    cout << "Enter 3x3 matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Adding diagonal elements
+    for (int i = 0; i < 3; i++) {
+        sum += matrix[i][i]; // main diagonal
+    }
+
+    cout << "\nSum of main diagonal elements: " << sum;
+
+    return 0;
+}
+
+// OUTPUT:
+
+Enter 3x3 matrix:
+1 2 3 
+4 5 6 
+7 8 9
+
+Sum of main diagonal elements: 15
