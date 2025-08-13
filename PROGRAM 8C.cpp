d5ab19324a1e9f035f36031926b6ffd774248104
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-13 16:36:28 +0530

    Update PROGRAM 8C.cpp

--- PROGRAM 8C.cpp
+++ PROGRAM 8C.cpp
@@ -1,3 +1,10 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: MULTIPLICATION OF MATRIX (EXPERIMENT: 8C)
+
+// CODE: 
+
 #include <iostream>
 using namespace std;
 
@@ -39,16 +46,16 @@ int main() {
 
 // OUTPUT: 
 
-Enter first 3x3 matrix:
-1 2 3 
-4 5 6
-7 8 9
-Enter second 3x3 matrix:
-9 8 7
-6 5 4
-3 2 1
+// Enter first 3x3 matrix:
+// 1 2 3 
+// 4 5 6
+// 7 8 9
+// Enter second 3x3 matrix:
+// 9 8 7
+// 6 5 4
+// 3 2 1
 
-Product of matrices:
-30 24 18 
-84 69 54 
-138 114 90 
+// Product of matrices:
+// 30 24 18 
+// 84 69 54 
+// 138 114 90 
